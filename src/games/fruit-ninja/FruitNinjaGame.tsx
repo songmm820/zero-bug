765ca96c76d6838a0f9d4606b19778add24c9dce
songmm <songmm@qingsongjituan.com>
2025-05-20 00:21:42 +0800

    feat: ✨ 水果忍者加载场景

--- src/games/fruit-ninja/FruitNinjaGame.tsx
+++ src/games/fruit-ninja/FruitNinjaGame.tsx
@@ -3,33 +3,20 @@
  * @author songmm
  */
 
-import { useEffect, useLayoutEffect, useRef } from 'react'
+import { useLayoutEffect, useRef } from 'react'
 import Phaser from 'phaser'
-import { useAtomValue } from 'jotai'
-import { appStateAtom } from '@/jotai-atoms/app-store'
 import startGame from '@/games/fruit-ninja/start-game'
 
 function FruitNinjaGame() {
   const game = useRef<Phaser.Game>()
 
-  // 获取APP原子信息
-  const appInfoAtom = useAtomValue(appStateAtom)
-
   useLayoutEffect(() => {
-    game.current = startGame('game-container', appInfoAtom.screenWidth, appInfoAtom.screenHeight)
+    game.current = startGame('game-container', 640, 480)
 
     return () => {
       game.current?.destroy(true)
     }
   }, [])
-
-  useEffect(() => {
-    // 监听屏幕尺寸变化，重新调整游戏窗口大小
-    if (!game) return
-    const { screenWidth, screenHeight } = appInfoAtom
-    game.current?.scale.resize(screenWidth, screenHeight)
-    Tools.Logger.info(`重新设置游戏窗口尺寸：${screenWidth} | ${screenHeight}`)
-  }, [appInfoAtom.screenWidth, appInfoAtom.screenHeight])
 
   return <div id="game-container"></div>
 }
--- src/games/fruit-ninja/scenes/boot-scene.ts
+++ src/games/fruit-ninja/scenes/boot-scene.ts
@@ -19,10 +19,34 @@ export class BootScene extends Phaser.Scene {
     STATIC_RESOURCES.forEach((item) => {
       this.load.image(item.key, item.url)
     })
-    this.add.image(0, 0, 'background')
   }
 
   create() {
     SceneUtils.fadeIn(this)
+    // 获取游戏的宽度和高度 700 * 550
+    const width = this.cameras.main.width
+    const height = this.cameras.main.height
+
+    this.homePending(width, height)
+  }
+
+  /**
+   * 待机动画
+   */
+  homePending(width: number, height: number) {
+    this.add.image(0, 0, 'background').setOrigin(0, 0).setDisplaySize(width, height)
+
+    const mask = this.add.image(0, -100, 'home-mask').setOrigin(0, 0).setDisplaySize(width, 200)
+
+    const logo = this.add.image(20, -100, 'logo').setOrigin(0, 0).setDisplaySize(200, 100)
+
+    this.tweens.add({
+      targets: [mask, logo],
+      y: 0,
+      duration: 400,
+      ease: 'Linear',
+      repeat: 0,
+      yoyo: false
+    })
   }
 }
