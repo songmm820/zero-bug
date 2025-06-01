47276d440cfbba19b5e8c78ebfd57a98eb3ed65b
SongMing <mmsong@yeah.net>
2025-06-01 18:01:03 +0800

    build: 📦 tauri 版本升级

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,22 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_os::init())
-        .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+async fn greet() -> String {
+    "Hello, World!".to_string()
 }
--- src/views/home/HomeMain.tsx
+++ src/views/home/HomeMain.tsx
@@ -1,13 +1,27 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { invoke } from '@tauri-apps/api/core'
 
 /**
  * Views：Main
  * @author songmm
  */
 function HomeMain() {
+  const [invokeRes, setInvokeRes] = useState<unknown>(null)
+
+  // 调用插件
+  async function callPlugin() {
+    const response = await invoke('greet', { name: 'tauri' })
+    setInvokeRes(response)
+  }
+
   // 获取当前网络局域网设备
   useEffect(() => {}, [])
-  return <main className="flex-1 p-4"></main>
+  return (
+    <main className="flex-1 p-4">
+      <div>{JSON.stringify(invokeRes)}</div>
+      <button onClick={() => callPlugin()}>调用插件</button>
+    </main>
+  )
 }
 
 export default HomeMain
