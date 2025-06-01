9d7f7f4b35cfbd9f006bc0e418a3c54009389e73
SongMing <mmsong@yeah.net>
2025-06-01 18:02:20 +0800

    build: 📦 tauri 版本升级

--- src/views/home/HomeView.tsx
+++ src/views/home/HomeView.tsx
@@ -3,14 +3,12 @@
  * @author songmm
  */
 
-import HomeHeader from './header/HomeHeader.tsx'
 import HomeMain from '@/views/home/HomeMain.tsx'
 
 function Home() {
   return (
     <>
       <main className="flex flex-col w-full h-full overflow-auto">
-        <HomeHeader />
         <HomeMain />
       </main>
     </>
